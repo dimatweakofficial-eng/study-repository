3306e21f54ae722a37eeeee9908756fb7e40a08e
Vidra Tweak <dimatweakofficial@gmail.com>
2026-01-16 16:37:49 +1000

    first commit

---
+++ feature1/feature.go
@@ -0,0 +1,7 @@
+package feature1
+
+import "github.com/fatih/color"
+
+func Cat() {
+	color.Cyan("Новая фишка - кот")
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"demo2/feature1"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Передаю руль коту")
+	feature1.Cat()
+}
