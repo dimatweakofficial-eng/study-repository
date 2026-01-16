7dd742790855b2b9d15a20f56175ebe866f77f9a
Vidra Tweak <dimatweakofficial@gmail.com>
2026-01-16 18:09:59 +1000

    third commit

---
+++ feature2/feature2.go
@@ -0,0 +1,7 @@
+package feature2
+
+import "github.com/fatih/color"
+
+func Dog() {
+	color.Green("О ебать, новая фишка подьехала. Это собака")
+}
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"demo2/feature1"
+	"demo2/feature2"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Передаю руль коту")
 	feature1.Cat()
+	feature2.Dog()
 }
