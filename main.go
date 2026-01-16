0f85843622bf10edbdbd2376be45e63c584193a7
Vidra Tweak <dimatweakofficial@gmail.com>
2026-01-16 18:00:09 +1000

    SecondFitch

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"demo2/feature1"
+	"demo2/feature2"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Передаю руль коту")
+	fmt.Println("Приложение")
 	feature1.Cat()
+	feature2.Dog()
 }
